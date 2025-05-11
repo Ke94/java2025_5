96031d3da1f54ebcb739cb7242e28a63e5401a0a
simon0x0 <70833874+simon0x0@users.noreply.github.com>
2025-05-11 15:20:20 +0800

    add Transaction model

---
+++ src/main/java/App/model/StorageManager.java
@@ -0,0 +1,5 @@
+package App.model;
+
+public class StorageManager {
+    public void readJson(){}
+}
---
+++ src/main/java/App/model/Transaction.java
@@ -0,0 +1,7 @@
+package App.model;
+
+public class Transaction {
+    int year, month, day;
+    KindOfTransaction kind;
+    int amount;
+}
