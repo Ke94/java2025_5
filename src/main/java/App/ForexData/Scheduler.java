8c8ca7c659af3df594bfac3c5c243ba8f535612c
Ke94 <candy79449@gmail.com>
2025-05-30 21:14:32 +0800

    修改analyzer

--- src/main/java/App/ForexData/Scheduler.java
+++ src/main/java/App/ForexData/Scheduler.java
@@ -8,11 +8,20 @@ import App.DesktopNotifier;
 public class Scheduler {
 
     public static void main(String[] args) {
+        StringBuilder result = new StringBuilder();
+        Map<String, List<Double>> data = new HashMap<>();
+
+        //fetch newest exchange data
         try {
-            Map<String, List<Double>> data = ForexFetcher.fetchAndAppendLatest();
-            DesktopNotifier.showMessage("匯率通知", openAIBot.getAdvice(ForexAnalyzer.analyze(data)));
+            data = ForexFetcher.fetchAndAppendLatest();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        for(String currency : data.keySet()){
+            result.append(ForexAnalyzer.analyze(data, currency));
+        }
+        DesktopNotifier.showMessage("匯率通知", openAIBot.getAdvice(result.toString()));
+
     }
 }
