d2884a2ba13843041366fce15270c71e05657861
Ke94 <candy79449@gmail.com>
2025-05-31 15:39:59 +0800

    新增通知事件

---
+++ src/main/java/App/ExchangeRateApp.java
@@ -0,0 +1,22 @@
+package App;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ExchangeRateApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/App/view/ExchangeRateView.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("匯率分析報告");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void launchApp() {
+        new Thread(() -> Application.launch(ExchangeRateApp.class)).start();
+    }
+}
